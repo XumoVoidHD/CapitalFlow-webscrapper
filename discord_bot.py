87f2a99e2e4f7c11a5e21ca28032de51a9da9d14
XumoVoidHD <vedanshsahay05@gmail.com>
2024-10-24 21:17:50 +0530

    changes

--- discord_bot.py
+++ discord_bot.py
@@ -5,12 +5,11 @@ import json
 
 
 def send_message(bot_token, user_id, signal, call, put, webhook_url, send_to_user=True, send_to_webhook=True):
-    # Set up the bot with a command prefix
-    intents = discord.Intents.default()
-    intents.message_content = True  # Enable message content intent
-    bot = commands.Bot(command_prefix="!", intents=intents)
 
-    # Event triggered when the bot is ready
+    intents = discord.Intents.default()
+    intents.message_content = True
+    bot = commands.Bot(command_prefix="?", intents=intents)
+
     @bot.event
     async def on_ready():
         print(f"Logged in as {bot.user}")
@@ -20,11 +19,10 @@ def send_message(bot_token, user_id, signal, call, put, webhook_url, send_to_use
         if send_to_webhook:
             send_message_to_webhook(webhook_url, signal, call, put)
 
-        await bot.close()  # Close the bot after sending the message
+        await bot.close()
 
-    # Function that sends a message to a user when triggered
     async def send_message_to_user(user_id):
-        user = await bot.fetch_user(user_id)  # Fetch the user object using the user ID
+        user = await bot.fetch_user(user_id)
         try:
             await user.send(f"Signal: {signal}\nCall: ${format(float(call), ',')}\nPut: ${format(float(put), ',')}")
             x = float(put)
@@ -35,7 +33,6 @@ def send_message(bot_token, user_id, signal, call, put, webhook_url, send_to_use
         except discord.Forbidden:
             print(f"Could not send message to {user.name} (they might have DMs disabled)")
 
-    # Function to send a message to a Discord webhook
     def send_message_to_webhook(webhook_url, signal, call, put):
         data = {
             "content": f"Signal: {signal}\nCall: ${format(float(call), ',')}\nPut: ${format(float(put), ',')}\nDifference: ${format(round(abs(float(put) - float(call)), 2), ',')}"
@@ -45,9 +42,8 @@ def send_message(bot_token, user_id, signal, call, put, webhook_url, send_to_use
         }
         response = requests.post(webhook_url, data=json.dumps(data), headers=headers)
         if response.status_code == 204:
-            print("Message sent to webhook successfully.")
+            print("Success")
         else:
-            print(f"Failed to send message to webhook. Status code: {response.status_code}")
+            print(f"Failed. Status code: {response.status_code}")
 
-    # Start the bot with your token
     bot.run(bot_token)
