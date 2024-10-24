5968cc8fb36535a2549b4400e1666963ec765216
XumoVoidHD <vedanshsahay05@gmail.com>
2024-10-24 21:38:05 +0530

    Delete tests directory

--- tests/main.py
+++
@@ -1,7 +0,0 @@
-from bs4 import BeautifulSoup
-import requests
-
-url = "https://dashboard.capitalflow.app/auth/login"
-res = requests.get(url)
-
-print(BeautifulSoup(res.text, features="html.parser"))
