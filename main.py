97c23f15d803e54fed548f0bcb892742dcd0d0fe
XumoVoidHD <vedanshsahay05@gmail.com>
2024-10-24 20:37:45 +0530

    Alerts implemented

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ import time
 from datetime import datetime
 from discord_bot import run_discord_bot
 
-token = ""
-user = ""
+token = "MTI5ODM4NjAxODA1MDkwMDA2MQ.GwVAKX.enia3A_dCmO-7BaBnP2dxu4du-i_Jj4ET41PPk"
+user = "481415673957056518"
 
 
 if 'email' not in st.session_state:
@@ -234,7 +234,7 @@ def run_scraper(email, password, queue):
     print(df)
 
 def send_msg(signal, call, put):
-    # run_discord_bot(bot_token=token, user_id=user, signal=signal, call=call, put=put)
+    run_discord_bot(bot_token=token, user_id=user, signal=signal, call=call, put=put)
 
 def driver():
     df = pd.read_csv("wow.csv")
